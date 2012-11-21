e6c54d4e01c8409fb22e67d1367728ed2482427a
Joshua Tacoma <joshua@yellowseed.org>
2012-11-21 14:42:02 -0500

    Partially implement TestBridge_RegisterIn.
    Also miscellaneous smaller changes.  Notice that the test does not yet
    bind or connect the sockets, and nothing is being properly closed.

--- bridge.go
+++ bridge.go
@@ -1,23 +1,40 @@
 package gzmq
 
 import (
+	"errors"
+
 	zmq "github.com/alecthomas/gozmq"
 )
 
 type Bridge struct {
-	Context zmq.Context
+	context zmq.Context
+}
+
+func NewBridge(context zmq.Context) (*Bridge, error) {
+	var err error
+	if context == nil {
+		context, err = zmq.NewContext()
+	}
+	if err != nil {
+		return nil, err
+	}
+	return &Bridge{context: context}, nil
+}
+
+func (b *Bridge) Context() zmq.Context {
+	return b.context
 }
 
 func (b *Bridge) RegisterIn(s zmq.Socket) (<-chan [][]byte, error) {
-	panic("TODO: implement this.")
+	return nil, errors.New("TODO: implement this.")
 }
 
 func (b *Bridge) RegisterOut(s zmq.Socket) (chan<- [][]byte, error) {
-	panic("TODO: implement this.")
+	return nil, errors.New("TODO: implement this.")
 }
 
-func (b *Bridge) RegisterInOut(s zmq.Socket) (<-chan [][]byte, chan<- [][]byte) {
-	panic("TODO: implement this.")
+func (b *Bridge) RegisterInOut(s zmq.Socket) (<-chan [][]byte, chan<- [][]byte, error) {
+	return nil, nil, errors.New("TODO: implement this.")
 }
 
 func (b *Bridge) Start() {
--- bridge_test.go
+++ bridge_test.go
@@ -2,26 +2,49 @@ package gzmq
 
 import (
 	"testing"
+	"time"
 
 	zmq "github.com/alecthomas/gozmq"
 )
 
-func (b *Bridge) TestBridge_RegisterIn(t *testing.T) {
+func TestBridge_RegisterIn(t *testing.T) {
+	unit, err := NewBridge(nil)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	pull, err := unit.Context().NewSocket(zmq.PULL)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	push, err := unit.Context().NewSocket(zmq.PUSH)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	pulling, err := unit.RegisterIn(pull)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	push.Send([]byte("test"), 0)
+	select {
+	case <-pulling:
+		t.Logf("received message.")
+	case <-time.After(10 * time.Millisecond):
+		t.Errorf("failed to receive message within timeout.")
+	}
+}
+
+func TestBridge_RegisterOut(t *testing.T) {
 	t.Errorf("TODO: test this.")
 }
 
-func (b *Bridge) TestBridge_RegisterOut(t *testing.T) {
+func TestBridge_RegisterInOut(t *testing.T) {
 	t.Errorf("TODO: test this.")
 }
 
-func (b *Bridge) TestBridge_RegisterInOut(t *testing.T) {
+func TestBridge_Start(t *testing.T) {
 	t.Errorf("TODO: test this.")
 }
 
-func (b *Bridge) TestBridge_Start(t *testing.T) {
-	t.Errorf("TODO: test this.")
-}
-
-func (b *Bridge) TestBridge_Stop(t *testing.T) {
+func TestBridge_Stop(t *testing.T) {
 	t.Errorf("TODO: test this.")
 }
