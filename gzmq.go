6441643329833971f619ad262117544e5f7a1a10
Joshua Tacoma <joshua@yellowseed.org>
2012-11-22 09:42:52 -0500

    Simplify: per-socket, not per-context.
    Following previous commit's suggestion: provide this functionality as a
    simpler extension of zmq.Socket.  Some thoughtful work remains to be
    done to get this correct, and further work to reduce the number of
    goroutines (stack traces are a already bit complex for only two
    sockets).

--- gzmq.go
+++ gzmq.go
@@ -6,248 +6,130 @@ import (
 	zmq "github.com/alecthomas/gozmq"
 )
 
-type Chan interface {
+type Socket interface {
 	Bind(address string) error
 	Connect(address string) error
-	Close() error
-	// TODO: methods to set zmq socket options
-}
-
-type In interface {
-	Chan
-	In() <-chan [][]byte
-}
-
-type Out interface {
-	Chan
-	Out() chan<- [][]byte
-}
-
-type InOut interface {
-	Chan
-	In() <-chan [][]byte
-	Out() chan<- [][]byte
-}
-
-type Context interface {
-	NewIn(s zmq.SocketType) (In, error)
-	NewOut(s zmq.SocketType) (Out, error)
-	NewInOut(s zmq.SocketType) (InOut, error)
-	Pump() error
+	Pump()
+	Recv() <-chan [][]byte
+	Send() chan<- [][]byte
 	Close() error
 }
 
-func NewContext() (Context, error) {
-	ctx, err := zmq.NewContext()
-	if err != nil {
-		return nil, err
-	}
-	return &context{
-		ctx:    ctx,
-		chans:  []Chan{},
-		events: make(chan event, 7),
-	}, nil
+type socket struct {
+	main    zmq.Socket    // the socket this struct abstracts
+	cmdSend zmq.Socket    // inproc for sending commands to pumpSock()
+	cmdRecv zmq.Socket    // inproc for receving commands in pumpSock()
+	recv    chan [][]byte // messages that have been received
+	send    chan [][]byte // messages waiting to be sent
+	cmd     chan [][]byte // commands to be processed
 }
 
-type context struct {
-	ctx    zmq.Context
-	chans  []Chan
-	events chan event
-}
-
-func (c *context) NewIn(t zmq.SocketType) (In, error) {
-	socket, err := c.ctx.NewSocket(t)
+func NewSocket(c zmq.Context, t zmq.SocketType) (Socket, error) {
+	main, err := c.NewSocket(t)
 	if err != nil {
 		return nil, err
 	}
-	enqueue, err := c.ctx.NewSocket(zmq.PUSH)
+	cmdSend, err := c.NewSocket(zmq.PUSH)
 	if err != nil {
 		return nil, err
 	}
-	dequeue, err := c.ctx.NewSocket(zmq.PULL)
+	cmdRecv, err := c.NewSocket(zmq.PULL)
 	if err != nil {
 		return nil, err
 	}
-	addr := newInprocAddress()
-	if err := enqueue.Bind(addr); err != nil {
+	cmdAddr := newInprocAddress()
+	err = cmdSend.Bind(cmdAddr)
+	if err != nil {
 		return nil, err
 	}
-	if err := dequeue.Connect(addr); err != nil {
+	err = cmdRecv.Connect(cmdAddr)
+	if err != nil {
 		return nil, err
 	}
-	result := &chanIn{
-		chanBase{
-			socket,
-			make(chan event, 7),
-		},
-		enqueue, dequeue,
-		make(chan [][]byte, 7),
+	result := &socket{
+		main:    main,
+		cmdSend: cmdSend,
+		cmdRecv: cmdRecv,
+		recv:    make(chan [][]byte, 2),
+		send:    make(chan [][]byte, 2),
+		cmd:     make(chan [][]byte, 2),
 	}
-	c.chans = append(c.chans, result)
 	return result, nil
 }
 
-func (c *context) NewOut(t zmq.SocketType) (Out, error) {
-	socket, err := c.ctx.NewSocket(t)
-	if err != nil {
-		return nil, err
-	}
-	result := &chanOut{
-		chanBase{
-			socket,
-			make(chan event, 7),
-		},
-		make(chan [][]byte, 7),
-	}
-	c.chans = append(c.chans, result)
-	return result, nil
+func (s *socket) Bind(address string) error    { return s.main.Bind(address) }
+func (s *socket) Connect(address string) error { return s.main.Connect(address) }
+func (s *socket) Recv() <-chan [][]byte        { return s.recv }
+func (s *socket) Send() chan<- [][]byte        { return s.send }
+
+func (s *socket) Close() error {
+	println("I: sending close to pumpChan()", s)
+	s.cmd <- [][]byte{[]byte{byte(closing)}}
+	return s.main.Close()
 }
 
-func (c *context) NewInOut(t zmq.SocketType) (InOut, error) {
-	socket, err := c.ctx.NewSocket(t)
-	if err != nil {
-		return nil, err
-	}
-	enqueue, err := c.ctx.NewSocket(zmq.PUSH)
-	if err != nil {
-		return nil, err
-	}
-	dequeue, err := c.ctx.NewSocket(zmq.PULL)
-	if err != nil {
-		return nil, err
-	}
-	addr := newInprocAddress()
-	if err := enqueue.Bind(addr); err != nil {
-		return nil, err
-	}
-	if err := dequeue.Connect(addr); err != nil {
-		return nil, err
-	}
-	return &chanInOut{
-		chanBase{
-			socket,
-			make(chan event, 7),
-		},
-		enqueue, dequeue,
-		make(chan [][]byte, 7),
-		make(chan [][]byte, 7),
-	}, nil
+func (s *socket) Pump() {
+	go s.pumpSock()
+	s.pumpChan()
+	println("I: closing socket", s)
+	s.main.Close()
+	println("I: closed socket", s)
 }
 
-func (c *context) Pump() error {
-	println("I: Pump() [start]")
-	for _, ch := range c.chans {
-		switch ch.(type) {
-		case *chanOut:
-			go ch.(*chanOut).pumpOut()
-		case *chanInOut:
-			go ch.(*chanInOut).pumpOut()
-		}
-		switch ch.(type) {
-		case *chanIn:
-			go ch.(*chanIn).pumpIn()
-		case *chanInOut:
-			go ch.(*chanInOut).pumpIn()
-		}
-	}
-	for e := range c.events {
-		switch e {
-		case closing:
-			for _, ch := range c.chans {
-				switch ch.(type) {
-				case *chanIn:
-					println("I: enqueuing close command for in")
-					ch.(*chanIn).enqueue.SendMultipart([][]byte{[]byte{byte(closing)}}, 0)
-				case *chanInOut:
-					ch.(*chanInOut).events <- e
-				case *chanOut:
-					ch.(*chanOut).events <- e
-				}
-			}
-			for _, ch := range c.chans {
-				switch ch.(type) {
-				case *chanIn:
-					ch.(*chanIn).enqueue.Close()
-				}
+// pumpChan pumps messages out from channels in to sockets.
+func (s *socket) pumpChan() {
+	defer s.cmdSend.Close()
+	var stopping = false
+	for !stopping {
+		println("I: pumpChan() back in the select again", s)
+		select {
+		case msg := <-s.send:
+			s.main.SendMultipart(msg, 0) // TODO: handle error
+		case e := <-s.cmd:
+			switch event(e[0][0]) {
+			case closing:
+				println("I: sending close to pumpSock()", s)
+				s.cmdSend.SendMultipart([][]byte{[]byte{byte(closing)}}, 0)
+				println("I: breaking pumpChan()", s)
+				stopping = true
 			}
 		}
 	}
-	println("I: closing zmq context...")
-	c.ctx.Close()
-	println("I: Pump() [end]")
-	return nil
+	println("I: closed pumpChan()", s)
 }
 
-func (c *context) Close() error {
-	c.events <- closing
-	c.ctx.Close()
-	return nil
-}
-
-type event int
-
-const (
-	_             = iota
-	closing event = iota
-)
-
-type chanBase struct {
-	socket zmq.Socket
-	events chan event
-}
-
-func (c *chanBase) Bind(address string) error {
-	return c.socket.Bind(address)
-}
-
-func (c *chanBase) Connect(address string) error {
-	return c.socket.Connect(address)
-}
-
-func (c *chanBase) Close() error {
-	// TODO: this is probably not robust...?
-	return c.socket.Close()
-}
-
-type chanIn struct {
-	chanBase
-	enqueue zmq.Socket
-	dequeue zmq.Socket
-	cin     chan [][]byte
-}
-
-func (c *chanIn) In() <-chan [][]byte { return c.cin }
-
-func (c *chanIn) pumpIn() {
+// pumpSock pumps messages in from sockets and out to channels.
+func (s *socket) pumpSock() {
+	defer s.cmdRecv.Close()
+	defer s.main.Close()
 	items := zmq.PollItems{
 		zmq.PollItem{
-			Socket: c.chanBase.socket,
+			Socket: s.cmdRecv,
 			Events: zmq.POLLIN,
 		},
-		zmq.PollItem{
-			Socket: c.dequeue,
+	}
+	typ, err := s.main.GetSockOptUInt64(zmq.TYPE)
+	if err != nil {
+		panic(err.Error())
+	}
+	switch zmq.SocketType(typ) {
+	case zmq.SUB, zmq.REQ, zmq.REP, zmq.DEALER, zmq.ROUTER, zmq.PULL:
+		items = append(items, zmq.PollItem{
+			Socket: s.main,
 			Events: zmq.POLLIN,
-		},
+		})
 	}
 	for {
 		_, err := zmq.Poll(items, -1)
 		if err == zmq.ETERM {
-			println("I: closing (in) due to context termination")
+			println("I: closing pumpSock() [context terminated]")
 			break
 		} else if err != nil {
 			panic(err.Error())
 		}
 		if (items[0].REvents & zmq.POLLIN) != 0 {
-			println("I: in received a message")
-			msg, err := c.chanBase.socket.RecvMultipart(0)
-			if err != nil {
-				panic(err.Error())
-			}
-			c.cin <- msg
-		}
-		if (items[1].REvents & zmq.POLLIN) != 0 {
 			println("I: in received an event")
-			msg, err := c.dequeue.RecvMultipart(0)
+			msg, err := s.cmdRecv.RecvMultipart(0)
 			if err != nil {
 				panic(err.Error())
 			}
@@ -256,72 +138,28 @@ func (c *chanIn) pumpIn() {
 			}
 			switch event(int(msg[0][0])) {
 			case closing:
-				println("I: closing (in)")
+				println("I: closing pumpSock()")
 				break
 			}
 		}
-	}
-	c.dequeue.Close()
-	c.socket.Close()
-}
-
-type chanOut struct {
-	chanBase
-	cout chan [][]byte
-}
-
-func (c *chanOut) Out() chan<- [][]byte { return c.cout }
-
-func (c *chanOut) pumpOut() {
-	println("I: pumping out...")
-	for {
-		select {
-		case msg := <-c.cout:
-			println("I: sending multipart message...")
-			c.socket.SendMultipart(msg, 0) // TODO: handle error
-			println("I: sent multipart message")
-		case e := <-c.events:
-			switch e {
-			case closing:
-				println("I: closing (out)")
-				c.socket.Close()
-				break
+		if len(items) > 1 && (items[1].REvents&zmq.POLLIN) != 0 {
+			println("I: in received a message")
+			msg, err := s.main.RecvMultipart(0)
+			if err != nil {
+				panic(err.Error())
 			}
-		}
-		println("I: pumped...")
-	}
-	println("I: closed.")
-}
-
-type chanInOut struct {
-	chanBase
-	enqueue zmq.Socket
-	dequeue zmq.Socket
-	cin     chan [][]byte
-	cout    chan [][]byte
-}
-
-func (c *chanInOut) In() <-chan [][]byte  { return c.cin }
-func (c *chanInOut) Out() chan<- [][]byte { return c.cout }
-
-func (c *chanInOut) pumpIn() {
-	panic("not implemented")
-}
-
-func (c *chanInOut) pumpOut() {
-	for {
-		select {
-		case msg := <-c.cout:
-			c.enqueue.SendMultipart(msg, 0) // TODO: handle error
-		case e := <-c.events:
-			switch e {
-			case closing:
-				// TODO!
-				break
-			}
+			s.recv <- msg
 		}
 	}
+	println("I: closed pumpSock()", s)
 }
+
+type event int
+
+const (
+	_             = iota
+	closing event = iota
+)
 
 var inprocNext = 1
 
--- gzmq_test.go
+++ gzmq_test.go
@@ -7,62 +7,42 @@ import (
 	zmq "github.com/alecthomas/gozmq"
 )
 
-func TestContext_PushPull(t *testing.T) {
+func TestSocket(t *testing.T) {
 	var (
-		unit Context
-		pull In
-		push Out
-		err  error
+		context zmq.Context
+		pull    Socket
+		push    Socket
+		err     error
 	)
-	if unit, err = NewContext(); err != nil {
+	if context, err = zmq.NewContext(); err != nil {
 		t.Fatalf(err.Error())
 	}
-	if pull, err = unit.NewIn(zmq.PULL); err != nil {
+	if pull, err = NewSocket(context, zmq.PULL); err != nil {
 		t.Fatalf(err.Error())
 	}
 	if err = pull.Bind("inproc://test"); err != nil {
 		t.Fatalf(err.Error())
 	}
-	if push, err = unit.NewOut(zmq.PUSH); err != nil {
+	if push, err = NewSocket(context, zmq.PUSH); err != nil {
 		t.Fatalf(err.Error())
 	}
 	if err = push.Connect("inproc://test"); err != nil {
 		t.Fatalf(err.Error())
 	}
-	go func() {
-		println("I: pumping context")
-		if err := unit.Pump(); err != nil {
-			t.Errorf(err.Error())
-		}
-		println("!!!")
-	}()
+	go pull.Pump()
+	go push.Pump()
 	time.Sleep(100 * time.Millisecond)
 	println("I: pushing out...")
-	push.Out() <- [][]byte{[]byte("test")}
+	push.Send() <- [][]byte{[]byte("test")}
 	println("I: pushed out")
 	select {
-	case <-pull.In():
+	case <-pull.Recv():
 		t.Logf("received message.")
 	case <-time.After(10 * time.Millisecond):
 		t.Errorf("failed to receive message within timeout.")
 	}
-	if err = unit.Close(); err != nil {
-		t.Fatalf(err.Error())
-	}
-}
-
-func TestContext_NewOut(t *testing.T) {
-	t.Errorf("TODO: test this.")
-}
-
-func TestContext_NewInOut(t *testing.T) {
-	t.Errorf("TODO: test this.")
-}
-
-func TestContext_Pump(t *testing.T) {
-	t.Errorf("TODO: test this.")
-}
-
-func TestContext_Close(t *testing.T) {
-	t.Errorf("TODO: test this.")
+	push.Close()
+	pull.Close()
+	println("I: closing context.")
+	context.Close()
 }
