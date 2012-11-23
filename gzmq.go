ec8e071d912ff3e8aa043bab8e872ff25646d51b
Joshua Tacoma <joshua@yellowseed.org>
2012-11-23 02:04:21 -0500

    New approach based on polling.
    Another overhaul: instead of wrapping sockets, wrap the loop that calls
    zmq.Poll().  This way it's just one goroutine even if there are many
    channels.  This is getting close to being the minimal work necessary to
    bridge sockets and channels.

--- gzmq.go
+++ gzmq.go
@@ -2,28 +2,112 @@ package gzmq
 
 import (
 	"strconv"
+	"sync"
 
 	zmq "github.com/alecthomas/gozmq"
 )
 
-type Socket interface {
-	Bind(address string) error
-	Connect(address string) error
-	Pump()
-	Recv() <-chan [][]byte
-	Send() chan<- [][]byte
+type Polling interface {
+	Include(zmq.Socket) (<-chan [][]byte, error)
 	Close() error
 }
 
-type socket struct {
-	main    zmq.Socket    // the socket this struct abstracts
-	msgSend zmq.Socket    //inproc for sending messages to pumpSock()
-	msgRecv zmq.Socket    //inproc for reciving messages in pumpSock()
-	cmdSend zmq.Socket    // inproc for sending commands to pumpSock()
-	cmdRecv zmq.Socket    // inproc for receving commands in pumpSock()
-	recv    chan [][]byte // messages that have been received
-	send    chan [][]byte // messages waiting to be sent
-	cmd     chan [][]byte // commands to be processed
+type polling struct {
+	notifySend zmq.Socket  // to notify goroutine of pending commands
+	commands   chan func() // pending commands
+	items      []pollingItem
+	closing    bool
+	closed     sync.WaitGroup
+}
+
+type pollingItem struct {
+	socket  zmq.Socket
+	channel chan [][]byte
+}
+
+func NewPolling(context zmq.Context) (Polling, error) {
+	notifySend, notifyRecv, err := newPair(context)
+	if err != nil {
+		return nil, err
+	}
+	p := polling{
+		notifySend: notifySend,
+		commands:   make(chan func(), 4),
+		items: []pollingItem{
+			pollingItem{notifyRecv, nil},
+		},
+	}
+	p.closed.Add(1)
+	go func() {
+		for !p.closing {
+			pollItems := make(zmq.PollItems, len(p.items))
+			for i, item := range p.items {
+				pollItems[i].Socket = item.socket
+				pollItems[i].Events = zmq.POLLIN
+			}
+			_, err := zmq.Poll(pollItems, -1)
+			if err != nil {
+				println("E:", err.Error())
+				break
+			}
+			if (pollItems[0].REvents & zmq.POLLIN) != 0 {
+				_, err := notifyRecv.RecvMultipart(0)
+				if err != nil {
+					println("E:", err.Error())
+					break
+				}
+				cmd := <-p.commands
+				cmd()
+			}
+			for i := 1; i < len(pollItems); i++ {
+				item := pollItems[i]
+				if (item.REvents & zmq.POLLIN) != 0 {
+					msg, err := item.Socket.RecvMultipart(0)
+					if err != nil {
+						println("E:", err.Error())
+						continue //?
+					}
+					pitem := p.items[i]
+					println("I: receiving message into channel.")
+					pitem.channel <- msg
+					println("I: received message into channel.")
+				}
+			}
+		}
+		notifyRecv.Close()
+		p.closed.Done()
+	}()
+	return &p, nil
+}
+
+func (p *polling) Close() error {
+	p.notifySend.Send([]byte{0}, 0)
+	p.notifySend.Close()
+	p.commands <- func() { p.closing = true }
+	p.closed.Wait()
+	return nil
+}
+
+func (p *polling) Include(s zmq.Socket) (result <-chan [][]byte, err error) {
+	done := make(chan int)
+	p.notifySend.Send([]byte{0}, 0)
+	p.commands <- func() {
+		var ok bool
+		for _, existing := range p.items {
+			if existing.socket == s {
+				ok = true
+			}
+		}
+		if !ok {
+			ch := make(chan [][]byte, 4)
+			println("I: including socket", s)
+			p.items = append(p.items, pollingItem{s, ch})
+			result = ch
+		}
+		done <- 1
+	}
+	<-done
+	return
 }
 
 func newPair(c zmq.Context) (send zmq.Socket, recv zmq.Socket, err error) {
@@ -46,151 +130,6 @@ func newPair(c zmq.Context) (send zmq.Socket, recv zmq.Socket, err error) {
 	}
 	return
 }
-
-func NewSocket(c zmq.Context, t zmq.SocketType) (Socket, error) {
-	main, err := c.NewSocket(t)
-	if err != nil {
-		return nil, err
-	}
-	cmdSend, cmdRecv, err := newPair(c)
-	if err != nil {
-		return nil, err
-	}
-	msgSend, msgRecv, err := newPair(c)
-	if err != nil {
-		return nil, err
-	}
-	result := &socket{
-		main:    main,
-		msgSend: msgSend,
-		msgRecv: msgRecv,
-		cmdSend: cmdSend,
-		cmdRecv: cmdRecv,
-		recv:    make(chan [][]byte, 2),
-		send:    make(chan [][]byte, 2),
-		cmd:     make(chan [][]byte, 2),
-	}
-	return result, nil
-}
-
-func (s *socket) Bind(address string) error    { return s.main.Bind(address) }
-func (s *socket) Connect(address string) error { return s.main.Connect(address) }
-func (s *socket) Recv() <-chan [][]byte        { return s.recv }
-func (s *socket) Send() chan<- [][]byte        { return s.send }
-
-func (s *socket) Close() error {
-	println("I: sending close to pumpChan()", s)
-	s.cmd <- [][]byte{[]byte{byte(closing)}}
-	return s.main.Close()
-}
-
-func (s *socket) Pump() {
-	go s.pumpSock()
-	s.pumpChan()
-	println("I: closing socket", s)
-	s.main.Close()
-	println("I: closed socket", s)
-}
-
-// pumpChan pumps messages out from channels in to sockets.
-func (s *socket) pumpChan() {
-	defer s.cmdSend.Close()
-	defer s.msgSend.Close()
-	var stopping = false
-	for !stopping {
-		println("I: pumpChan() back in the select again", s)
-		select {
-		case msg := <-s.send:
-			s.msgSend.SendMultipart(msg, 0) // TODO: handle error
-		case e := <-s.cmd:
-			switch event(e[0][0]) {
-			case closing:
-				println("I: sending close to pumpSock()", s)
-				s.cmdSend.SendMultipart([][]byte{[]byte{byte(closing)}}, 0)
-				println("I: breaking pumpChan()", s)
-				stopping = true
-			}
-		}
-	}
-	println("I: closed pumpChan()", s)
-}
-
-// pumpSock pumps messages in from sockets and out to channels.
-func (s *socket) pumpSock() {
-	defer s.main.Close()
-	defer s.msgRecv.Close()
-	defer s.cmdRecv.Close()
-	items := zmq.PollItems{
-		zmq.PollItem{
-			Socket: s.cmdRecv,
-			Events: zmq.POLLIN,
-		},
-		zmq.PollItem{
-			Socket: s.msgRecv,
-			Events: zmq.POLLIN,
-		},
-	}
-	typ, err := s.main.GetSockOptUInt64(zmq.TYPE)
-	if err != nil {
-		panic(err.Error())
-	}
-	switch zmq.SocketType(typ) {
-	case zmq.SUB, zmq.REQ, zmq.REP, zmq.DEALER, zmq.ROUTER, zmq.PULL:
-		items = append(items, zmq.PollItem{
-			Socket: s.main,
-			Events: zmq.POLLIN,
-		})
-	}
-	for {
-		_, err := zmq.Poll(items, -1)
-		if err == zmq.ETERM {
-			println("I: closing pumpSock() [context terminated]")
-			break
-		} else if err != nil {
-			println("E: unknown error, breaking socket reading loop:", err)
-			break
-		}
-		if (items[0].REvents & zmq.POLLIN) != 0 {
-			println("I: in received an event")
-			msg, err := s.cmdRecv.RecvMultipart(0)
-			if err != nil {
-				panic(err.Error())
-			}
-			if len(msg[0]) == 0 {
-				continue
-			}
-			switch event(int(msg[0][0])) {
-			case closing:
-				println("I: closing pumpSock()")
-				break
-			}
-		}
-		if (items[1].REvents & zmq.POLLIN) != 0 {
-			println("I: in received a message to send")
-			msg, err := s.msgRecv.RecvMultipart(0)
-			if err != nil {
-				panic(err.Error())
-			}
-			s.main.SendMultipart(msg, 0)
-		}
-		if len(items) > 2 && (items[2].REvents&zmq.POLLIN) != 0 {
-			println("I: in received a message")
-			msg, err := s.main.RecvMultipart(0)
-			if err != nil {
-				panic(err.Error())
-			}
-			s.recv <- msg
-		}
-	}
-	println("I: closed pumpSock()", s)
-}
-
-type event int
-
-const (
-	_             = iota
-	closing event = iota
-)
 
 var inprocNext = 1
 
--- gzmq_test.go
+++ gzmq_test.go
@@ -10,37 +10,39 @@ import (
 func TestSocket(t *testing.T) {
 	var (
 		context zmq.Context
-		pull    Socket
-		push    Socket
+		pull    zmq.Socket
+		push    zmq.Socket
+		polling Polling
+		cpull   <-chan [][]byte
 		err     error
 	)
 	if context, err = zmq.NewContext(); err != nil {
 		t.Fatalf(err.Error())
 	}
-	if pull, err = NewSocket(context, zmq.PULL); err != nil {
+	if pull, err = context.NewSocket(zmq.PULL); err != nil {
 		t.Fatalf(err.Error())
 	}
 	if err = pull.Bind("inproc://test"); err != nil {
 		t.Fatalf(err.Error())
 	}
-	if push, err = NewSocket(context, zmq.PUSH); err != nil {
+	if push, err = context.NewSocket(zmq.PUSH); err != nil {
 		t.Fatalf(err.Error())
 	}
 	if err = push.Connect("inproc://test"); err != nil {
 		t.Fatalf(err.Error())
 	}
-	go pull.Pump()
-	go push.Pump()
-	time.Sleep(100 * time.Millisecond)
+	polling, err = NewPolling(context)
 	println("I: pushing out...")
-	push.Send() <- [][]byte{[]byte("test")}
+	push.Send([]byte("test"), 0)
 	println("I: pushed out")
+	cpull, err = polling.Include(pull)
 	select {
-	case <-pull.Recv():
+	case <-cpull:
 		t.Logf("received message.")
 	case <-time.After(10 * time.Millisecond):
 		t.Errorf("failed to receive message within timeout.")
 	}
+	polling.Close()
 	push.Close()
 	pull.Close()
 	println("I: closing context.")
