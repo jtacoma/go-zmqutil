23a51b8ed554645cdb0830e36bdcf4db29d18861
Joshua Tacoma <joshua@yellowseed.org>
2012-11-23 21:12:09 -0500

    Use defer to close in tests.

--- gzmq_test.go
+++ gzmq_test.go
@@ -19,19 +19,23 @@ func TestPolling(t *testing.T) {
 	if context, err = zmq.NewContext(); err != nil {
 		t.Fatalf(err.Error())
 	}
+	defer context.Close()
 	if pull, err = context.NewSocket(zmq.PULL); err != nil {
 		t.Fatalf(err.Error())
 	}
+	defer pull.Close()
 	if err = pull.Bind("inproc://test"); err != nil {
 		t.Fatalf(err.Error())
 	}
 	if push, err = context.NewSocket(zmq.PUSH); err != nil {
 		t.Fatalf(err.Error())
 	}
+	defer push.Close()
 	if err = push.Connect("inproc://test"); err != nil {
 		t.Fatalf(err.Error())
 	}
 	polling, err = NewPolling(context)
+	defer polling.Close()
 	push.Send([]byte("test"), 0)
 	cpull, err = polling.Include(pull)
 	select {
@@ -39,16 +43,6 @@ func TestPolling(t *testing.T) {
 	case <-time.After(10 * time.Millisecond):
 		t.Errorf("failed to receive message within timeout.")
 	}
-	if err = polling.Close(); err != nil {
-		t.Fatalf(err.Error())
-	}
-	if err = push.Close(); err != nil {
-		t.Fatalf(err.Error())
-	}
-	if err = pull.Close(); err != nil {
-		t.Fatalf(err.Error())
-	}
-	context.Close()
 }
 
 func TestPolling_Sync(t *testing.T) {
@@ -62,19 +56,23 @@ func TestPolling_Sync(t *testing.T) {
 	if context, err = zmq.NewContext(); err != nil {
 		t.Fatalf(err.Error())
 	}
+	defer context.Close()
 	if reQ, err = context.NewSocket(zmq.REQ); err != nil {
 		t.Fatalf(err.Error())
 	}
+	defer reQ.Close()
 	if err = reQ.Bind("inproc://test"); err != nil {
 		t.Fatalf(err.Error())
 	}
 	if reP, err = context.NewSocket(zmq.REP); err != nil {
 		t.Fatalf(err.Error())
 	}
+	defer reP.Close()
 	if err = reP.Connect("inproc://test"); err != nil {
 		t.Fatalf(err.Error())
 	}
 	polling, err = NewPolling(context)
+	defer polling.Close()
 	if creP, err = polling.Include(reP); err != nil {
 		t.Fatalf(err.Error())
 	}
@@ -94,14 +92,4 @@ func TestPolling_Sync(t *testing.T) {
 			done = true
 		}
 	}
-	if err = polling.Close(); err != nil {
-		t.Fatalf(err.Error())
-	}
-	if err = reP.Close(); err != nil {
-		t.Fatalf(err.Error())
-	}
-	if err = reQ.Close(); err != nil {
-		t.Fatalf(err.Error())
-	}
-	context.Close()
 }
