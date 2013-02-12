38a229a99d06ea88189f7f85e8a8ab05a1fa6213
Joshua Tacoma <joshua@yellowseed.org>
2013-02-12 08:42:15 -0500

    Add package synopsis in new doc.go.

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Copyright 2013 Joshua Tacoma. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package gzmq implements some ØMQ abstractions and utilities.
+package gzmq
